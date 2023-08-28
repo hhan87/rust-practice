9d34fd3beb037ead9af31e75e6753fa1da7b0070
hhan87 <hhan87@gmail.com>
2023-08-29 00:32:04 +0900

    add listener loop

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 pub struct Server {
     addr: String,
 }
@@ -12,5 +13,26 @@ impl Server {
     pub fn run(self){
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
+        loop{
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(size) =>{
+                            println!("read conn: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => println!("Error reading: {}", e)
+
+                    }
+                    
+                },
+                Err(e) =>{
+                    println!("Failed to establish a conn: {}", e);
+                    continue 
+                }
+            }
+        }
+        
+
     }
 }
